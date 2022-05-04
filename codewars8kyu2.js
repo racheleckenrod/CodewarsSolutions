de2f7312511327761b66449e7ea64e5567e2a9c9
racheleckenrod <rachel@racheleckenrod.com>
2022-05-04 06:42:45 -0500

    Codewars and coffee

---
+++ codewars8kyu2.js
@@ -0,0 +1,22 @@
+/*
+Make a function that will return a greeting statement that uses 
+an input; your program should return,
+ "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the 
+    program may not execute properly]
+    */
+
+/*    function greet(name){
+        //your code here
+        name = prompt("Please enter your name:")
+        return ("Hello, " + name + " how are you doing today?");
+      }
+ */
+      function greet(name){
+        //your code here
+        
+              return ("Hello, " + name + " how are you doing today?");
+      }
+
+    
