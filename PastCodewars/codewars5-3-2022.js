bb0792985702fc897341cf7393b1f6efecf11184
racheleckenrod <rachel@racheleckenrod.com>
2022-05-06 19:32:48 -0500

    organization

---
+++ PastCodewars/codewars5-3-2022.js
@@ -0,0 +1,10 @@
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+function checkForFactor (base, factor) {
+  return(base % factor===0)
+  }
+
+//   tried to use if base % factor === 0 return true, and else 
+// false, but that didn't work. This version makes more sense to me too.
