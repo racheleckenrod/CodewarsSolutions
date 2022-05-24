a76dc724e5069240edb540710418f3f51c471596
racheleckenrod <rachel@racheleckenrod.com>
2022-05-24 08:30:46 -0500

    variables

---
+++ Codewars/codewars40.js
@@ -0,0 +1,10 @@
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+var a = "dev"
+var b = "Lab"
+
+let = name 
+name = a + b
+
+// looks like I may have just gotten luck here, as I wasn't aware of the issues with the term "name" and it's depricatoin.
