e6b40b02478702c6a46d5983f3652a9a44c85db1
racheleckenrod <rachel@racheleckenrod.com>
2022-05-27 15:37:05 -0500

    second try

---
+++ Codewars/codewars52.js
@@ -0,0 +1,10 @@
+// Implement a function which multiplies two numbers.
+
+
+// Write here your multiply-function
+
+function multiply(a,b){
+    return a * b
+}
+
+// somehow I was supposed to know that the function was supposed to be named "multiply"... I guess I had better start looking at the tests more carefully if I want to get more first tries..
