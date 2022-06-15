1eff81acb7f6040bd5483043d02c23ca46ffbae0
racheleckenrod <rachel@racheleckenrod.com>
2022-06-15 12:15:24 -0500

    first try

---
+++ Codewars/codewars124.js
@@ -0,0 +1,6 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+// had two == instead of one.
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
