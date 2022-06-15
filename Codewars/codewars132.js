4105b69b4c45cd822c5c4a167da7cb3b4ee2d8e3
racheleckenrod <rachel@racheleckenrod.com>
2022-06-15 13:44:29 -0500

    first try

---
+++ Codewars/codewars132.js
@@ -0,0 +1,22 @@
+// Return the Nth Even Number
+
+// Example(Input --> Output)
+
+// 1 --> 0 (the first even number is 0)
+// 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+// 100 --> 198
+// 1298734 --> 2597466
+// The input will not be 0.
+// what is the tenth even number? (0,2,4,6,8,10,12,14,16,18)
+1-0
+2-2
+3-4
+4-6
+5-8
+6-10
+7-12
+
+function nthEven(n){
+    return (n*2) - 2
+    // your code here
+  }
