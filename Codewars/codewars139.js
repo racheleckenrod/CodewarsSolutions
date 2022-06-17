622c899b574abb04012150cf54daae288c0bcc48
racheleckenrod <rachel@racheleckenrod.com>
2022-06-16 20:30:43 -0500

    second try

---
+++ Codewars/codewars139.js
@@ -0,0 +1,15 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+// FUND
+
+function opposite(number) {
+    return number * (-1)
+  }
+
+//   oh I did not know it could be even simplier-
+// return -number
