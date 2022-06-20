1083bbbd1a23b2e32fe41657d837d58b27f8f116
racheleckenrod <rachel@racheleckenrod.com>
2022-06-20 11:33:47 -0500

    couple wishful thinking tries before doing it the long way

---
+++ Codewars/codewars154.js
@@ -0,0 +1,25 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+// pseudocode- I dont know if the template literal notation will work with this one, but that is what cam e to me first to try so I will. OK so not so much... 
+
+function basicOp(operation, value1, value2){
+  if(operation === '+'){
+    return value1 + value2
+  }else if(operation === '-'){
+    return value1 - value2
+  }else if(operation === '*'){
+    return value1 * value2
+  }else if(operation === '/'){
+    return value1 / value2
+  }
+}
+
+console.log(basicOp('+', 4, 7))
