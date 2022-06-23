835f6e5bbe4ee878b34211a2bdd90621b3c11288
racheleckenrod <rachel@racheleckenrod.com>
2022-06-23 13:47:40 -0500

    couple tries

---
+++ Codewars/codewars168.js
@@ -0,0 +1,19 @@
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+// For example (Input --> Output)
+
+// "Yes" --> "Yes?" 
+// "No?" --> "No?"
+
+function ensureQuestion(s) {
+    // Code here
+    s.endsWith('?')? s.push("?") : s
+  }
+
+  function ensureQuestion(s){
+    if(s.endsWith('?')){
+        return s
+    }else{
+        return s.concat('','?')
+    }
+  }
