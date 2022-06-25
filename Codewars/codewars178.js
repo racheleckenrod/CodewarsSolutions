e0783795fba490c4ece593ab8c333bf033eea97e
racheleckenrod <rachel@racheleckenrod.com>
2022-06-25 10:01:59 -0500

    happy with second try

---
+++ Codewars/codewars178.js
@@ -0,0 +1,14 @@
+// It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+// Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+// If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+// Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell and Lua) or "¥" (Rust).
+
+function bonusTime(salary, bonus) {
+    // your code here
+    return bonus ? "\u00A3" + 10*salary : "\u00A3" + salary
+    }
+
+    // woo-hoo got this on the second try... didn't use a return with the ternary, but I did get the ternary part correct... progress!!
