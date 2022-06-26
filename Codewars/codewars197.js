ffb137c39f112c03de64361a8480f2dd00e4c418
racheleckenrod <rachel@racheleckenrod.com>
2022-06-26 17:58:55 -0500

    my else if solution would have probably be a good candidate for at least a switch case although I saw many object solutions for this kata

---
+++ Codewars/codewars197.js
@@ -0,0 +1,35 @@
+// Complete the function that receives as input a string, and produces outputs according to the following table:
+
+// Input	Output
+// "Jabroni"	"Patron Tequila"
+// "School Counselor"	"Anything with Alcohol"
+// "Programmer"	"Hipster Craft Beer"
+// "Bike Gang Member"	"Moonshine"
+// "Politician"	"Your tax dollars"
+// "Rapper"	"Cristal"
+// anything else	"Beer"
+// Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+
+// Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+
+function getDrinkByProfession(param){
+    param = param.toLowerCase()
+    if(param === "jabroni"){
+        return "patron tequila"
+    }else if(param ===  "school counselor"){
+        return 	"anything with alcohol"
+    }else if(param === "programmer"){
+        return "Hipster Craft Beer"
+    }else if(param === "bike gang member"){
+        return "Moonshine"
+    }else if(param ===  "politician"){
+        return "Your tax dollars"
+    }else if (param === "rapper"){
+        return "Cristal"
+    }else{
+        return "Beer"
+    }
+
+}
+
