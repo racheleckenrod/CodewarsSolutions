1c1d8b2adb18047fb23091ae521dba53b1f946a4
racheleckenrod <rachel@racheleckenrod.com>
2022-07-01 16:12:41 -0500

    first try

---
+++ Codewars/codewars244.js
@@ -0,0 +1,7 @@
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+// PREP
+// Parameters- I assume that list is an array, 
+
+function include(arr, item){
+    return arr.includes(item)
+  }
