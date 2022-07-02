86945da264cbd96c3fb49881c1c82388fb76b2b1
racheleckenrod <rachel@racheleckenrod.com>
2022-07-02 18:40:01 -0500

    glad I did not give up on this one, but it took many tries ended up being an empty array

---
+++ Codewars/codewars257.js
@@ -0,0 +1,17 @@
+// You task to pass only this tests :
+
+// a == false
+// !a == false
+// a == !a
+
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals( a == false,  true, `a != false`);
+//   Test.assertEquals( !a == false, true, `!a != false`);
+//   Test.assertEquals( a == !a,     true, `a != !a`);
+//     });
+//   });
+  
+
+
+const a = []
