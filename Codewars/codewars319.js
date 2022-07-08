2840b6638b4547cbe9514f12fa52f2fdc25f9722
racheleckenrod <rachel@racheleckenrod.com>
2022-07-07 20:00:02 -0500

    bailed on this one and looked at the solution and it was very easy and had more than one easy answer

---
+++ Codewars/codewars319.js
@@ -0,0 +1,10 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+websites[0] = "codewars"
+
+// I added the [] after first attempt was showing the length of 8 instead of one
