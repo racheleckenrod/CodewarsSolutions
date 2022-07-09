9971da8c773ca4bf6957e6955ca9886c11c1e9f0
racheleckenrod <rachel@racheleckenrod.com>
2022-07-09 11:56:48 -0500

    lots of different solutions

---
+++ Codewars/codewars343.js
@@ -0,0 +1,33 @@
+// Here is a piece of code:
+
+// function getStatus(isBusy) {
+//   var msg = (isBusy ? "busy" : "available");
+//   return 
+//   {
+//     status: msg
+//   }
+// }
+// Expected Behaviour
+// Function should return a dictionary/Object/Hash with "status" as a key, whose value can : "busy" or "available" depending on the truth value of the argument is_busy.
+
+// But as you will see after clicking RUN or ATTEMPT this code has several bugs, please fix them.
+
+
+function getStatus(isBusy) {
+    let obj = {}
+        obj.status = (isBusy ? "busy" : "available");
+        return obj
+    }
+
+    // another way to fix this code:
+    // function getStatus(isBusy) {
+    //     var msg = (isBusy ? "busy" : "available");
+    //     return {
+    //       status: msg
+    //     };
+    //   }
+
+    // Also this:
+    // function getStatus(isBusy) {
+//   return { status: isBusy ? "busy" : "available" }
+// }
