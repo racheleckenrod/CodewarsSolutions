435ac01e4e457d6a212be2d35b8bddb8584d6a2f
racheleckenrod <rachel@racheleckenrod.com>
2022-07-09 14:43:30 -0500

    lots of help on this one

---
+++ Codewars/codewars352.js
@@ -0,0 +1,13 @@
+function Warrior(n){
+    let name = n;  
+    this.name = function(n){
+      if( n ) name=n;
+      return name;
+    }
+  }
+   
+      Warrior.prototype.toString = function(){
+          return `Hi! my name's ${this.name()}`
+      }
+  
+  
