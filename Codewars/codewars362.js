caae7658a1f8ba7c327bc36f54c338511b6b3028
racheleckenrod <rachel@racheleckenrod.com>
2022-07-09 20:09:26 -0500

    fun

---
+++ Codewars/codewars362.js
@@ -0,0 +1,8 @@
+// The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+// You may need to think laterally!
+
+
+console.log(blackBox.toString(JSopenSesame()))
+
+// just calling JSopenSesame() worked to open the box.. the toString() method  gave us a message in the testing area
