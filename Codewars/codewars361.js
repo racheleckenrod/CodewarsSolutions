6d69f164391e6c843b60a6fc60591367d7a74ba2
racheleckenrod <rachel@racheleckenrod.com>
2022-07-09 19:25:24 -0500

    updates to shortening the code

--- Codewars/codewars361.js
+++ Codewars/codewars361.js
@@ -40,3 +40,10 @@ class Labrador extends Dog{
       this.loyal = true;
     }
   }
+
+//   ok at how it can be shortened further!!
+// class Labrador extends Dog {
+//     constructor(name, age, gender, master) {
+//       super(name, age, gender, "Labrador", "Large", master, true);
+//     }
+//   }
