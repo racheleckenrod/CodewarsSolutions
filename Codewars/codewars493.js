69ceca1926a3e6621070bc6dd302b775df28570d
racheleckenrod <rachel@racheleckenrod.com>
2022-07-27 12:47:40 -0500

    much simplier solutions than I came up with

---
+++ Codewars/codewars493.js
@@ -0,0 +1,35 @@
+// Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+
+// Notes
+// If the array is null/nil/None or empty you should return empty array ([]).
+// The rotten fruit name will be in this camelcase (rottenFruit).
+// The returned array should be in lowercase.
+
+
+
+function removeRotten(bagOfFruits){
+    for(let i = 0; i < bagOfFruits.length; i++){
+        console.log(bagOfFruits[i])
+        if(bagOfFruits[i].toLowerCase() !== bagOfFruits[i]){
+            bagOfFruits[i] = bagOfFruits[i][6].toLowerCase() + bagOfFruits[i].slice(7,bagOfFruits[i].length)
+        }
+    }
+    return bagOfFruits
+  }
+
+  console.log(removeRotten(["apple","rottenBanana","apple"]))
+
+
+//   sure did a lot of extra work on this one
+
+// function removeRotten(arr){
+//     return arr ? arr.map(x=>x.replace('rotten', '').toLowerCase()) : [] ;
+// }
+
+// or
+
+// function removeRotten(bagOfFruits){
+//     if(!bagOfFruits || !bagOfFruits.length) {return [];} 
+//     else {return bagOfFruits.map(e => e.replace(/^rotten/g, '').toLowerCase())};
+//   }
+  
