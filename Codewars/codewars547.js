7457d93de48bdc6bdedba48de80124ce2cd3f59f
racheleckenrod <rachel@racheleckenrod.com>
2022-08-07 21:44:32 -0500

    second try

---
+++ Codewars/codewars547.js
@@ -0,0 +1,13 @@
+// Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+
+function bump(x){
+    x = x.split("")
+    let y = x.filter((item) => item === 'n')
+    if(y <= 15){
+        return 'Woohoo!'
+    }else{
+        return 'Car Dead'
+    }
+}
