766d5ad365dd435707af83d3411d0ec8f2a11065
racheleckenrod <rachel@racheleckenrod.com>
2022-09-01 17:30:43 -0500

    strings

---
+++ Codewars/codewars615.js
@@ -0,0 +1,28 @@
+// Oh no!
+// Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+
+// You need to cast the whole array to the correct type.
+
+// Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+
+// ie:["1", "2", "3"] to [1, 2, 3]
+
+// Note that you can receive floats as well.
+
+
+function toNumberArray(stringarray){
+    for(let i = 0; i < stringarray.length; i++){
+      stringarray[i] = Number(stringarray[i])
+    }
+      return stringarray
+  }
+
+//   a good solution :
+
+// const toNumberArray = stringarray => stringarray.map(Number);
+
+// or something like this:
+
+// function toNumberArray(stringarray){
+//     return stringarray.map(el => Number(el))
+//   }
