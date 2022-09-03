cd71d963e1e817f728cef9342e64ca971fe6a4ea
racheleckenrod <rachel@racheleckenrod.com>
2022-09-03 17:58:08 -0500

    no need to use this

---
+++ Codewars/codewars620.js
@@ -0,0 +1,15 @@
+// Define a method/function that removes from a given array of integers all the values contained in a second array.
+
+// Examples (input -> output):
+// * [1, 1, 2, 3, 1, 2, 3, 4], [1, 3] -> [2, 2, 4]
+// * [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6, 7, 8]
+// * [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
+// Enjoy it!!
+
+// PREP
+// This is a prototype create a method problem. which will most likely need to use the .this to select the given input. I dont know how to do a for loop with the .this
+
+Array.prototype.remove_ = function(integer_list, values_list){
+   integer_list.filter(element => !values_list.includes(element))
+  }
+  
