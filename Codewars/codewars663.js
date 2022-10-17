df784d2b9925d42f0ab95612d3f4dfd22129a037
racheleckenrod <rachel@racheleckenrod.com>
2022-10-17 14:48:44 -0500

    small additional comments reguarding setting things up to ignore the DS Store business

--- Codewars/codewars663.js
+++ Codewars/codewars663.js
@@ -1,1 +1,2 @@
 // this is going to be a test case first
+// adding the ignore config commands again, I guess it is for the repo, I thought it was for my whole git before, but maybe not. Find out more what the global means exactly
