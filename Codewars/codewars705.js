754da14e3032cb5155ac73a4d1a04456b2d02943
racheleckenrod <rachel@racheleckenrod.com>
2022-10-25 10:11:33 -0500

    completed coding challange

---
+++ Codewars/codewars705.js
@@ -0,0 +1,18 @@
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+// GEOMETRYFUNDAMENTALS
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(angle(3), 180);
+//     assert.strictEqual(angle(4), 360);
+//   });
+// })
+
+
+function angle(n) {
+    return (n-2) * 180
+  }
