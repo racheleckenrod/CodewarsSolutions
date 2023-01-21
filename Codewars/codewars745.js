6be57c0e85bdb4e74c389ce3835b6894f6ec01f4
racheleckenrod <rachel@racheleckenrod.com>
2023-01-21 05:16:22 -0600

    completed coding challenge

---
+++ Codewars/codewars745.js
@@ -0,0 +1,35 @@
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+
+
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     //some test cases for you...
+//     assert.isTrue(isValidWalk(['n','s','n','s','n','s','n','s','n','s']), 'should return true');
+//     assert.isFalse(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']), 'should return false');
+//     assert.isFalse(isValidWalk(['w']), 'should return false');
+//     assert.isFalse(isValidWalk(['n','n','n','s','n','s','n','s','n','s']), 'should return false');
+
+//   });
+// });
+
+// pseudocode:
+// number of n === s && w === e && .length === 10
+
+function isValidWalk(walk) {
+   let n = walk.filter(item => item === "n")
+   let s = walk.filter(item => item === "s")
+   let e = walk.filter(item => item === "e")
+   let w = walk.filter(item => item === "w")
+
+   return walk.length === 10 && n.length === s.length && e.length === w.length
+    //insert brilliant code here
+  }
+  console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']))
