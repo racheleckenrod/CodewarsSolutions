00bd234584de94bb84336ca7642cbc440633c3f1
racheleckenrod <rachel@racheleckenrod.com>
2023-01-26 19:24:38 -0600

    completed coding challenge

---
+++ Codewars/codewars758.js
@@ -0,0 +1,23 @@
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+
+var uniqueInOrder=function(iterable){
+    if(typeof iterable === String){
+        iterable = iterable.split("")
+    }
+    let result = []
+    for(let i = 0; i < iterable.length; i++){
+      
+        if(iterable[i + 1] !== iterable[i]){
+            result.push(iterable[i])
+        }
+    }
+    return result
+}
+console.log(uniqueInOrder([1,2,2,2,3,3,4,5]))
