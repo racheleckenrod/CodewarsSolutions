f4c05d4810bb15ab624243e9658f45529b31837a
racheleckenrod <rachel@racheleckenrod.com>
2023-02-16 08:18:21 -0600

    completed coding challenge

---
+++ Codewars/codewars784.js
@@ -0,0 +1,10 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+// PREP
+// to return even numbers use %2
+
+function noOdds( values ){
+    return values.filter(num => num % 2 === 0)
+  }
