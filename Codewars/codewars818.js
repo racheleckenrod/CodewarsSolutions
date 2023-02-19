65ad26dd3332f734adf1088872725e006eddef7a
racheleckenrod <rachel@racheleckenrod.com>
2023-02-19 13:56:53 -0600

    completed coding challenge

---
+++ Codewars/codewars818.js
@@ -0,0 +1,26 @@
+// source: imgur.com
+// The code provided is supposed return a person's Full Name given their first and last names.
+
+// But it's not working properly.
+
+// Notes
+// The first and/or last names are never null, but may be empty.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// Test.assertEquals(new Dinglemouse("Clint", "Eastwood").getFullName(), "Clint Eastwood");
+
+
+class Dinglemouse{
+
+    constructor( firstName, lastName ){
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+    
+    getFullName(){
+      return (this.firstName+" "+this.lastName).trim()
+    }
+    
+  }
