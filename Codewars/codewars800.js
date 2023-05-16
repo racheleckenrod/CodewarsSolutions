6a0b874daac292c1c44c6b86e52624c06e698474
racheleckenrod <rachel@racheleckenrod.com>
2023-05-16 15:11:03 -0500

    completed coding challenge

---
+++ Codewars/codewars800.js
@@ -0,0 +1,25 @@
+// An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+// Example:
+
+// The starting array sequence is [1,2,3,4,5,6,7,8,9]
+// The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+// Your function should return the int 5.
+// If no number was deleted from the starting array, your function should return the int 0.
+
+// Note: N may be 1 or less (in the latter case, the first array will be []).
+
+
+function findDeletedNumber(arr, mixArr) {
+    
+    if(arr.length === mixArr.length) return 0
+    mixArr.sort((a, b) => a - b)
+    console.log(mixArr)
+    for(let i =0; i < mixArr.length +1; i++){
+        if(i + 1 != mixArr[i]){
+            return i + 1
+        }
+    }
+  }
+
+  console.log(findDeletedNumber([1,2,3,4,5], [ 4, 1, 2, 3 ]))
