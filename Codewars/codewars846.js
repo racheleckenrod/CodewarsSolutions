ca1138344adcd4d9612757843cef187eb9c219fb
racheleckenrod <rachel@racheleckenrod.com>
2023-06-28 06:41:18 -0500

    completed coding challenge

--- Codewars/codewars846.js
+++ Codewars/codewars846.js
@@ -16,7 +16,34 @@
 //   });
 // });
 
+// PREP
+// P: we are given a string
+// R: we are to return a string containing a letter that is the first non repeating letter. it shouls be the same case as the given letter even though repeating characters can be of either case.
+// E: given "STress" we should return "T"
+// P: to return the proper case, we will find the answer with a .toLowerCase() string and return it using the original string. loop through the letters to look for more of them, and if not find any, return the letter at the position from original string. use Set to get a string of unique values and return the character first not in the Set string
+
 
 function firstNonRepeatingLetter(s) {
-    // Add your code here
+    let arr = s.toLowerCase().split('')
+    let repeats = []
+
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i + 1; j < arr.length; j++){
+
+            if( arr[i] === arr[j]){
+                console.log('i=',i,'j=',j)
+                repeats.push(arr[i])
+            }
+        }
+    }
+    console.log(repeats)
+    // compare repeats with lowercase array
+   for(let i = 0; i < arr.length; i++){
+    if(!repeats.includes(arr[i])) {
+        return s[i]
+    }
+   }
+   return ''
   }
+
+  console.log(firstNonRepeatingLetter('moonmendd'))
