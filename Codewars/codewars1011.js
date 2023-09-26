6f7a01270452c8a0f028008d9631ce50b4bb4862
racheleckenrod <rachel@racheleckenrod.com>
2023-09-26 16:49:53 -0400

    completed coding challenge

---
+++ Codewars/codewars1011.js
@@ -0,0 +1,23 @@
+// Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+// The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+// Example:
+
+// solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+
+
+function solution(pairs){
+    console.log(pairs)
+
+    let string = []
+
+    for (const key in pairs) {
+        if (pairs.hasOwnProperty(key)) {
+            string.push(`${key} = ${obj[key]}`)
+        }
+    }
+
+    return `${string.join(", ")}`
+    // TODO: complete
+  }
