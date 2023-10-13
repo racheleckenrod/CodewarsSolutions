1c9f85784e07584770f5da45d09477c0dfb43e61
racheleckenrod <rachel@racheleckenrod.com>
2023-10-13 11:23:58 -0400

    completed coding challenge

---
+++ Codewars/codewars1030.js
@@ -0,0 +1,8 @@
+// Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
+
+// REGULAR EXPRESSIONSFUNDAMENTALS
+
+
+String.prototype.isLetter = function() {
+    return /^[a-z]$/i.test(this)
+  }
