c2cce731f34ce366028173a0d6de1cd8fb80a8f7
racheleckenrod <rachel@racheleckenrod.com>
2024-01-09 15:50:53 -0500

    completed coding challenge

---
+++ Codewars/codewars1064.js
@@ -0,0 +1,51 @@
+// It's time to create an autocomplete function! Yay!
+
+// The autocomplete function will take in an input string and a dictionary array and return the values from the dictionary that start with the input string. If there are more than 5 matches, restrict your output to the first 5 results. If there are no matches, return an empty array.
+
+// Example:
+
+// autocomplete('ai', ['airplane','airport','apple','ball']) = ['airplane','airport']
+// For this kata, the dictionary will always be a valid array of strings. Please return all results in the order given in the dictionary, even if they're not always alphabetical. The search should NOT be case sensitive, but the case of the word should be preserved when it's returned.
+
+// For example, "Apple" and "airport" would both return for an input of 'a'. However, they should return as "Apple" and "airport" in their original cases.
+
+// Important note:
+
+// Any input that is NOT a letter should be treated as if it is not there. For example, an input of "$%^" should be treated as "" and an input of "ab*&1cd" should be treated as "abcd".
+
+// (Thanks to wthit56 for the suggestion!)
+
+// STRINGSREGULAR EXPRESSIONSARRAYSFUNDAMENTALS
+
+// // Since Node 10, we're using Mocha.
+// // You can use `chai` for assertions.
+// const chai = require("chai");
+// const assert = chai.assert;
+// // Uncomment the following line to disable truncating failure messages for deep equals, do:
+// // chai.config.truncateThreshold = 0;
+// // Since Node 12, we no longer include assertions from our deprecated custom test framework by default.
+// // Uncomment the following to use the old assertions:
+// // const Test = require("@codewars/test-compat");
+
+// describe("Solution", function() {
+//   it("should test for something", function() {
+//     // Test.assertEquals(1 + 1, 2);
+//     // assert.strictEqual(1 + 1, 2);
+//   });
+// });
+
+
+function autocomplete(input, dictionary){
+    let count = 0;
+    input = input.replace(/[^a-zA-Z]/g, '');
+    return dictionary.filter(item => {
+        if (item.toLowerCase().startsWith(input.toLowerCase()) && count < 5) {
+            count++;
+            return true;
+        }
+        return false;
+    })
+  
+}
+
+console.log(autocomplete("ab",["apple", "banana", "Abacus", "able", "absolute", "be", "car"] ))
